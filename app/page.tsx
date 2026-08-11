155fa02f41ab12b47ed6d4f0ffc28ace35d4bf77
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-11 11:18:35 +0200

    Mount post-class evaluation gate

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import CyaApp from "./cya-app";
+import { EvaluationPostClassGate } from "./evaluation-post-class";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <CyaApp />;
+  return <><CyaApp /><EvaluationPostClassGate /></>;
 }
