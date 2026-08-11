eed28ebb5b6871b65dbafaaccbc389922fa3cee6
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-11 11:34:21 +0200

    Add evaluation history route

---
+++ app/evaluation-history/page.tsx
@@ -0,0 +1,7 @@
+import EvaluationHistoryClient from "./evaluation-history-client";
+
+export const dynamic = "force-dynamic";
+
+export default function EvaluationHistoryPage() {
+  return <EvaluationHistoryClient />;
+}
