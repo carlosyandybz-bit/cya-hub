2e634641d654f50929cc990cf13198c4ec626d0d
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-11 11:42:08 +0200

    Add real progress summary route

---
+++ app/progress-summary/page.tsx
@@ -0,0 +1,7 @@
+import ProgressSummaryClient from "./progress-summary-client";
+
+export const dynamic = "force-dynamic";
+
+export default function ProgressSummaryPage() {
+  return <ProgressSummaryClient />;
+}
