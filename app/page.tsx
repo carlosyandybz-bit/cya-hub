e7f22e57142ee0e50659e8784852879d944e4104
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-11 12:37:44 +0200

    Mount guided initial evaluation during classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import CyaApp from "./cya-app";
+import { InitialEvaluationClassGate } from "./evaluation-initial-class";
 import { EvaluationPostClassGate } from "./evaluation-post-class";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <><CyaApp /><EvaluationPostClassGate /></>;
+  return <><CyaApp /><InitialEvaluationClassGate /><EvaluationPostClassGate /></>;
 }
