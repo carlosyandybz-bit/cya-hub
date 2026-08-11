a423dfb320cc12d042a1285175514f54f7f712d9
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-11 11:19:01 +0200

    Add evaluation settings route

---
+++ app/evaluation-settings/page.tsx
@@ -0,0 +1,7 @@
+import EvaluationSettingsClient from "./evaluation-settings-client";
+
+export const dynamic = "force-dynamic";
+
+export default function EvaluationSettingsPage() {
+  return <EvaluationSettingsClient />;
+}
