8755aad2e8fd8922c5ec7825629fff98cc438eae
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-12 04:53:54 +0200

    fix: prepare dedicated post-class evaluation sessions

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import CyaApp from "./cya-app";
 import { InitialEvaluationClassGate } from "./evaluation-initial-class";
 import { EvaluationPostClassGate } from "./evaluation-post-class";
+import { EvaluationPostClassPreparer } from "./evaluation-post-class-preparer";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <><CyaApp /><InitialEvaluationClassGate /><EvaluationPostClassGate /></>;
+  return <><CyaApp /><InitialEvaluationClassGate /><EvaluationPostClassPreparer /><EvaluationPostClassGate /></>;
 }
