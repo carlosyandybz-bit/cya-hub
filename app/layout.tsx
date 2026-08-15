fd57a4a29c73f1bf3a95f6fe3d038c76979c9b71
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-15 23:12:23 +0200

    feat(p36): rebase professor home visual layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ import "./prf3-admin-navigation.css";
 import "./aud017-regression-fixes.css";
 import "./aud020-student-experience.css";
 import "./cya-game-ux-system.css";
+import "./p36-professor-home.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
