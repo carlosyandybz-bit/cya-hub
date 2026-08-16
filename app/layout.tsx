bddb08f68b1cb9be0b4b560bd5ef098719125061
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-16 16:28:17 +0200

    feat(ux): load collision-safe professor header last

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ import "./p36-admin-teaching.css";
 import "./cya-detail-system-v2.css";
 import "./cya-bottom-navigation-v38.css";
 import "./mobile-alignment-fix-v43.css";
+import "./professor-header-v44.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
