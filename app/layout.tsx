1fc62db79d65b7d71b2c94dadae09f96530c3e93
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-18 16:07:14 +0200

    Load approved staging UI polish in production-safe sync

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,8 @@ import "./cya-watermark-fidelity-v55.css";
 import "./cya-student-portal-identity-v56.css";
 import "./cya-module-color-purity-v57.css";
 import "./cya-module-interaction-states-v58.css";
+import "./cya-ui-polish-v59.css";
+import "./teaching-block-b.css";
 import "./pull-to-refresh.css";
 
 const geistSans = Geist({
