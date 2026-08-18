92067a5c346cf6d5846caf4f55f75e2e0cb6966f
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-18 04:36:15 +0200

    staging: load module interaction states v58

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,7 @@ import "./cya-premium-watermarks-v54.css";
 import "./cya-watermark-fidelity-v55.css";
 import "./cya-student-portal-identity-v56.css";
 import "./cya-module-color-purity-v57.css";
+import "./cya-module-interaction-states-v58.css";
 import "./pull-to-refresh.css";
 
 const geistSans = Geist({
