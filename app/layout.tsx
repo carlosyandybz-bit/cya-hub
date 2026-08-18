f744c5ca511c0ba9f51d24d9e02126b4c88716c5
carlosyandybz-bit <carlosyandybz@gmail.com>
2026-08-18 03:54:42 +0200

    staging: load student portal identity v56

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ import "./cya-module-identity-v52.css";
 import "./cya-watermark-quality-v53.css";
 import "./cya-premium-watermarks-v54.css";
 import "./cya-watermark-fidelity-v55.css";
+import "./cya-student-portal-identity-v56.css";
 import "./pull-to-refresh.css";
 
 const geistSans = Geist({
